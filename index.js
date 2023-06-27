3f30fbaa5e840dc1762ffa79b8fba1bde62e01c6
Carlota Bergel <carlotabergel7@hotmail.com>
2023-06-27 22:51:57 +0200

    nueva funcion

---
+++ index.js
@@ -0,0 +1,31 @@
+function sum (op1, op2=0)
+{
+    let resultado = op1 + op2
+    return resultado;
+}
+
+function subs (op1, op2=0)
+{
+    let resultado = op1 - op2
+    return resultado;
+}
+
+function mult (op1, op2=0)
+{
+    let resultado = op1 * op2
+    return resultado;
+}
+
+function div (op1, op2=0)
+{
+    let resultado = op1 / op2
+    return resultado;
+}
+
+function cuadrado (n)
+{
+    let resultado = Math.sqrt(n);
+    return resultado
+}
+
+module.exports= {sum, subs, mult, div, cuadrado}
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index.js");
+
+console.log(myLib.sum(2,3));
+console.log(myLib.subs(8,4));
+console.log(myLib.mult(3,5));
+console.log(myLib.div(10,5));
+console.log(myLib.cuadrado(64))
